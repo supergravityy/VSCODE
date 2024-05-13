8396010647f480545dc08da0a1e3063b45beb3c6
supergravityy <dbtjdtn325@gmail.com>
2024-05-13 14:42:29 +0900

    Try34_

--- Try34.cpp
+++ Try34.cpp
@@ -24,5 +24,15 @@ double Rectangle::Area(/*프라이빗 안의 멤버를 사용하기에 안써도 상관없다*/)
 
 int main()
 {
+    double width, height;
 
+    cout << "input your rectange spec : ";
+    cin >> width >> height;
+    cout << endl;
+
+    Rectangle myRect(width, height);
+
+    cout << "The Area :" << myRect.Area();
+
+    return 0;
 }
